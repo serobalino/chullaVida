f7354a2a79192efff04adb32f32a0fc506bedf56
SebastiaN <odm_sd@hotmail.com>
2019-01-11 02:03:44 -0500

    tabla reglas

---
+++ app/Regla.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Regla extends Model
+{
+    protected $primaryKey       =       "id_re";
+    public $timestamps          =       false;
+}
