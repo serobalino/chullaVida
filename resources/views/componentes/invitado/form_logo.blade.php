adf2587e9fdfac6692b732742157ed9f911efd5a
SebastiaN <odm_sd@hotmail.com>
2019-01-12 13:21:03 -0500

    componenetes

---
+++ resources/views/componentes/invitado/form_logo.blade.php
@@ -0,0 +1,5 @@
+<div class="m-login__logo">
+    <a href="{{route('home')}}">
+        <img src="{{asset('images/logo.svg')}}" alt="{{config('app.name')}} LOGO" style="height: 70px;max-height: 70px">
+    </a>
+</div>
---
+++ resources/views/componentes/invitado/registro.blade.php
@@ -0,0 +1,23 @@
+<div class="m-stack__item m-stack__item--center">
+    <div class="m-login__account">
+        <a href="{{route('log.linkedin')}}" class="btn btn-outline-brand btn-sm m-btn m-btn--icon">
+            <span>
+                <i class="fa fa-linkedin-square"></i>
+                <span>linkedin</span>
+            </span>
+        </a>
+        <a href="{{route('log.google')}}" class="btn btn-outline-brand btn-sm 	m-btn m-btn--icon">
+            <span>
+                <i class="fa fa-google-plus"></i>
+                <span>Google</span>
+            </span>
+        </a>
+        <br>
+        <span class="m-login__account-msg">
+            No tiene una cuenta?
+        </span>
+        <a href="{{route('register')}}" class="m-link m-link--focus m-login__account-link">
+            Regístrese
+        </a>
+    </div>
+</div>
