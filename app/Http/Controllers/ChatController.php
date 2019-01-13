4180eb65fa22167e38fb16a25d90dbd0970293b8
SebastiaN <odm_sd@hotmail.com>
2019-01-13 01:49:29 -0500

    comentario den chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -9,6 +9,10 @@ class ChatController extends Controller
 {
     private  $url="http://sandbox.api.simsimi.com/request.p";
 
+    /**
+     * Consulta por GET a la api de SIM SIMI y lanza un numero aleatorio
+     * de 3 a 25 segundo para q demore en responder el mensaje enviado
+    **/
     public function enviarMsg(Request $datos){
         Consulta::verifyPeer(false);
         $response = Consulta::get($this->url,
