bc40f369c2237266a9a5fce774d2ad41ae770a80
SebastiaN <odm_sd@hotmail.com>
2019-01-12 23:31:05 -0500

    compoenente chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,10 +17,8 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
 Vue.component('juegos', require('./components/juegos').default);
+Vue.component('chat', require('./components/chat').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
