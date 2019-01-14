0e3c530bc86f427361a06165c93c4df2acbadf80
SebastiaN <odm_sd@hotmail.com>
2019-01-14 00:10:02 -0500

    cambio de logica de reglas

--- app/Http/Controllers/GenerarReglasController.php
+++ app/Http/Controllers/GenerarReglasController.php
@@ -10,33 +10,40 @@ use Illuminate\Support\Facades\Auth;
 
 class GenerarReglasController extends Controller
 {
+    /**
+     * devuelve la vista de reglas
+    **/
     public function jugar(){
         return view('juego');
     }
 
-
+    /**
+     *
+     * Genera Consulta y Registra reglas al azar por el nombre de jugadores ingresado
+    **/
     public function generar(){
         $partida    =   Auth::user()->juego();
         if($partida){
-            $anterior   =   Log::where('id_ju',$partida->id_ju)->latest()->first();
-            $regla      =   collect(Regla::where('id_ti',$partida->id_ti)->where('id_re','!=',@$anterior->id_re)->get())->random(1)->first();
-            if(!$regla)
-                $regla  = Regla::where('id_ti',$partida->id_ti)->first();
+            $anterior   =   Log::where('id_ju',$partida->id_ju)->latest()->first();//consulta cual fue la anterior regla echa
+            $reglas     =   Regla::where('id_ti',$partida->id_ti)->where('id_re','!=',@$anterior->id_re)->get();//consulta la reglas posible diferentes a la ultima regla echa
+            if(count($reglas))
+                $regla  =   collect($reglas)->random(1)->first();//regla al azar del consulto posible
+            else
+                $regla  = Regla::where('id_ti',$partida->id_ti)->first();//misma regla si no existan mas
 
-            $tomado =   collect($partida->jugadores)->random(1)->first();
-            $sobra  =   collect($partida->jugadores)->filter(function ($value) use($tomado){
+            $tomado =   collect($partida->jugadores)->random(1)->first();//toma el nombre de un jugador al azar
+            $sobra  =   collect($partida->jugadores)->filter(function ($value) use($tomado){//quita el nombre del jugador tomado de la lista
                 return $value['id_pa']!==$tomado->id_pa;
             });
 
-            if(strpos($regla->detalle_re,'x'))
-                $texto  =   str_replace('x',$tomado->nombre_pa,$regla->detalle_re);
-            if(strpos($regla->detalle_re,'y')){
-                $tomado =   collect($sobra)->random(1)->first();
-                $texto  =   str_replace('y',$tomado->nombre_pa,$texto);
+            if(strpos($regla->detalle_re,'x'))//busca el parametro x en la regal
+                $texto  =   str_replace('x',$tomado->nombre_pa,$regla->detalle_re);//si encuentra remplaza por el nombre del jugador tomado
+            if(strpos($regla->detalle_re,'y')){//busca y en la regla
+                $tomado =   collect($sobra)->random(1)->first();//si existe toma el nombre de un jugador
+                $texto  =   str_replace('y',$tomado->nombre_pa,$texto);//remplaza por el nombre dle jugador
             }
 
-            $nueva  =   new Log();
-
+            $nueva  =   new Log();//crea un registro
             $nueva->id_ju             =   $partida->id_ju;
             $nueva->id_re             =   $regla->id_re;
             $nueva->instruccion_re    =   $texto;
@@ -47,6 +54,10 @@ class GenerarReglasController extends Controller
             return response(['val'=>false,'mensaje'=>'No se puede generar Reglas porque no tiene un juego activo']);
     }
 
+    /**
+     * cambia el estado de un Juego a desactivado
+     *
+    **/
     public function parar(){
         $partida            =   Auth::user()->juego();
         $partida->estado_ju =   false;
