bcae2e0fb5e6a33c3607ec15294416d43b036641
SebastiaN <odm_sd@hotmail.com>
2019-01-13 21:40:18 -0500

    registro de links de sns

--- resources/views/componentes/invitado/registro.blade.php
+++ resources/views/componentes/invitado/registro.blade.php
@@ -1,18 +1,17 @@
 <div class="m-stack__item m-stack__item--center">
     <div class="m-login__account">
-        {{--
-        <a href="{{route('log.linkedin')}}" class="btn btn-outline-brand btn-sm m-btn m-btn--icon">
+        <a href="{{route('github')}}" class="btn btn-outline-danger btn-sm m-btn m-btn--icon">
             <span>
-                <i class="fa fa-linkedin-square"></i>
-                <span>linkedin</span>
+                <i class="fa fa-github"></i>
+                <span>GitHub</span>
             </span>
         </a>
-        <a href="{{route('log.google')}}" class="btn btn-outline-brand btn-sm 	m-btn m-btn--icon">
+        <a href="{{route('google')}}" class="btn btn-outline-danger btn-sm 	m-btn m-btn--icon">
             <span>
                 <i class="fa fa-google-plus"></i>
                 <span>Google</span>
             </span>
-        </a>--}}
+        </a>
         <br>
         <span class="m-login__account-msg">
             No tiene una cuenta?
